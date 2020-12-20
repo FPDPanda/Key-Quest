ba1908b6082252c62e178c97b89f2143785fabe7
FPDPanda <jeanjacques7.jj@gmail.com>
2020-12-20 15:25:17 -0300

    🐛  Fixing bugs (⚔️ Combat) - Fixing main stats (explore.js)

--- scripts/main_stats.js
+++ scripts/main_stats.js
@@ -1,12 +1,10 @@
-let stats = {
+// When the window loads this call the function "setStats"
+window.addEventListener("load", setStats({
     health: 5,
     maxHealth: 5,
     bag: [],
     weapons: ['dagger']
-};
-
-// When the window loads this call the function "setStats"
-window.addEventListener("load", setStats(stats));
+}));
 
 // This is the function that will set all the stats, then it turns this function into an empty function NOOP, making it so the main stats will only be set once.
 function setStats(stats) {
