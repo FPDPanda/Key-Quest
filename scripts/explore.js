48dccb103fa6dede85d7f8c99ba23920bd623a2e
FPDPanda <jeanjacques7.jj@gmail.com>
2020-12-21 22:47:29 -0300

    ⚔️ Combat - Changing combat
    Combat message going from 'alert' to a new 'p' tag

--- scripts/explore.js
+++ scripts/explore.js
@@ -1,13 +1,27 @@
 let stats = JSON.parse(localStorage.getItem("stats"));
+let boxText = document.getElementById("box__text");
+let addP = document.createElement("p");
+addP.setAttribute("style", "color: red");
+let addedText = "";
 
-const button__continue = document.getElementById("button__continue");
 
-button__continue.addEventListener("click", function() {
+const buttonContinue = document.getElementById("button__continue");
+
+buttonContinue.addEventListener("click", function() {
     fight();
 });
 
 function fight() {
     stats.health--;
-    alert("Your health is now " + stats.health);
+    addedText = document.createTextNode("You were hurt!");
+    console.log(addP.classList);
+    addtext();
+    addedText = document.createTextNode("Your health is now " + stats.health);
+    addtext();
     localStorage.setItem("stats", JSON.stringify(stats));
 };
+
+function addtext() {
+    boxText.appendChild(addP.appendChild(addedText));
+    boxText.appendChild(document.createElement("br"));
+}
