b0b565cca72ad042c3d528c4a05d8e63ee3a5639
FPDPanda <jeanjacques7.jj@gmail.com>
2020-12-21 23:19:42 -0300

    ⚔️ Combat - Making changes to combat message
    Changing combat messages so that it will only happen once, and the color of the text "You were hurt" is red

--- scripts/explore.js
+++ scripts/explore.js
@@ -1,27 +1,39 @@
 let stats = JSON.parse(localStorage.getItem("stats"));
+
 let boxText = document.getElementById("box__text");
-let addP = document.createElement("p");
-addP.setAttribute("style", "color: red");
-let addedText = "";
+let addHurt = document.createElement("p");
+addHurt.classList.add('red');
+let addHealth = document.createElement("p");
+
+let counter = 0;
 
 
 const buttonContinue = document.getElementById("button__continue");
 
 buttonContinue.addEventListener("click", function() {
-    fight();
+    if (counter === 0) {
+        fight();
+    }   
+    else if (counter === 1) {
+        fight_result();
+    }
 });
 
 function fight() {
     stats.health--;
-    addedText = document.createTextNode("You were hurt!");
-    console.log(addP.classList);
-    addtext();
-    addedText = document.createTextNode("Your health is now " + stats.health);
-    addtext();
+    addHurt.textContent = "You were hurt!";
+    addtext(addHurt);
+    addHealth.textContent = "Your health is now " + stats.health;
+    addtext(addHealth);
     localStorage.setItem("stats", JSON.stringify(stats));
 };
 
-function addtext() {
-    boxText.appendChild(addP.appendChild(addedText));
+function addtext(text) {
     boxText.appendChild(document.createElement("br"));
+    boxText.appendChild(text);
+}
+
+function fight_result(){
+
+
 }
