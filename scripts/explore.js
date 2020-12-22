9b5e21809000f2ca6d7a270318cdf91e9457d27e
FPDPanda <jeanjacques7.jj@gmail.com>
2020-12-22 18:31:43 -0300

    💬 Comments - Adding comments to explore.js
    To make readability better

--- scripts/explore.js
+++ scripts/explore.js
@@ -1,23 +1,30 @@
+// Importing stats from main_stats.js
 let stats = JSON.parse(localStorage.getItem("stats"));
 
+// Creating the variables used in the functions fight() and addText()
 let boxText = document.getElementById("box__text");
 let addHurt = document.createElement("p");
 addHurt.classList.add('red');
 let addHealth = document.createElement("p");
 
+// Initializing a counter as 0
 let counter = 0;
 
-
+// Getting the button from the HTML
 const buttonContinue = document.getElementById("button__continue");
 
 buttonContinue.addEventListener("click", function() {
+
+    // This happens on the first time user clicks the button
     if (counter === 0) {
         fight();
         counter++
     }   
+
+    // This happens on the second time user clicks the button
     else if (counter === 1) {
         fight_result();
-        counter++    
+        counter++
     }
 });
 
@@ -30,12 +37,12 @@ function fight() {
     localStorage.setItem("stats", JSON.stringify(stats));
 };
 
-function addtext(text) {
+function addText(text) {
     boxText.appendChild(document.createElement("br"));
     boxText.appendChild(text);
 }
 
-function fight_result(){
+function fightResult(){
 
 
 }
