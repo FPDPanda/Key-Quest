7356a0f025d2a67723451560474ebcca791ffcae
FPDPanda <jeanjacques7.jj@gmail.com>
2020-12-24 14:30:57 -0300

    🖌️ Frontend (MAIN PAGE) - Adding functionality to the (HEALTH ❤️)

--- scripts/main_page.js
+++ scripts/main_page.js
@@ -1,3 +1,12 @@
 // Importing stats from main_stats.js
 let stats = JSON.parse(localStorage.getItem("stats"));
 
+// Getting the stats from the stats bar
+let displayedHealth = document.getElementById("player__stats__health");
+
+window.addEventListener("load", function() {
+    if (stats.health < stats.maxHealth) {
+        displayedHealth.classList.add('red');
+    }
+    displayedHealth.textContent = stats.health;
+});
--- scripts/tavern.js
+++ scripts/tavern.js
@@ -1,3 +1,4 @@
+// Importing stats from main_stats.js
 stats = JSON.parse(localStorage.getItem("stats"));
 
 // Getting the button TAVERN from the HTML
@@ -7,4 +8,8 @@ buttonTavern.addEventListener("click", function() {
     stats.health = stats.maxHealth;
     localStorage.setItem('stats', JSON.stringify(stats));
     alert("You restored your HP to MAX!");
+    
+    displayedHealth.classList.remove('red');
+    // ERASE THIS WHEN YOU CREATE A SEPARATE PAGE FOR THE TAVERN
+    displayedHealth.textContent = stats.health;
 });
