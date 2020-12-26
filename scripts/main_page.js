3eb4e3c43e3ddfc3047dc7eda478627dcb76d496
FPDPanda <jeanjacques7.jj@gmail.com>
2020-12-26 16:28:54 -0300

    👨‍💻 Scripting - Renaming a variable and adding a style
    - Renamed variable "player__weapon__stats__text" to simply "player__weapon__stats"
    
    - Added class "grey" to player__weapon__stats

--- scripts/main_page.js
+++ scripts/main_page.js
@@ -3,12 +3,14 @@ let stats = JSON.parse(localStorage.getItem("stats"));
 
 // Getting the stats locations from the stats bar
 let displayedHealth = document.getElementById("player__stats__health");
-let displayedWeapon = document.getElementById("player__stats__weapon__text");
+let displayedWeapon = document.getElementById("player__stats__weapon");
 
 window.addEventListener("load", function() {
     if (stats.health < stats.maxHealth) {
         displayedHealth.classList.add('red');
     }
+
+    displayedWeapon.classList.add('grey')
 
     displayedHealth.textContent = stats.health;
     displayedWeapon.textContent = stats.weapons[stats.weapons.length -1];
