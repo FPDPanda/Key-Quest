6fbb84405e48ba520d833d85148ee38913a59a8c
FPDPanda <jeanjacques7.jj@gmail.com>
2021-01-09 17:48:52 -0300

    💬 Comments - All comments from the language area

--- scripts/main_language.js
+++ scripts/main_language.js
@@ -1,12 +1,23 @@
+// -------------- LANGUAGES AREA -------------- //
+
+// Getting the languages area
 let language = document.getElementById("languages")
+
+// Getting the language buttons
+//Portuguese
 let portuguese = document.getElementById("pt-br");
+//English
 let english = document.getElementById("en");
+
+// This is what will be put in localStorage
 let chosenLanguage = "";
 
+// To make sure the language area will only appear once
 if(localStorage.language) {
     language.style.display="none";
 }
 
+// If the player selects Portuguese
 portuguese.addEventListener("click", function() {
     chosenLanguage = "portuguese"
     language.style.display="none";
@@ -14,6 +25,7 @@ portuguese.addEventListener("click", function() {
 
 });
 
+// If the player selects English
 english.addEventListener("click", function() {
     chosenLanguage = "english"
     language.style.display="none";
@@ -21,6 +33,8 @@ english.addEventListener("click", function() {
 
 });
 
+// Updates the chosen language and puts it in localStorage
 function setLanguage(chosenLanguage) {
     localStorage.getItem("language")? 0 : localStorage.setItem("language", JSON.stringify(chosenLanguage));
 }
+// -------------- END OF LANGUAGES AREA -------------- //
