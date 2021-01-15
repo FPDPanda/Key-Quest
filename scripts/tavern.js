b96051669f6c7b996537ce1e07469d7194ef8a36
FPDPanda <jeanjacques7.jj@gmail.com>
2021-01-15 14:15:09 -0300

    👨‍💻 Scripting (TAVERN) - Adding the language packs

--- scripts/tavern.js
+++ scripts/tavern.js
@@ -2,13 +2,28 @@
 /* *** DO LET STATS WHEN YOU CREATE A SEPARATE PAGE FOR THE TAVERN */
 stats = JSON.parse(localStorage.getItem("stats"));
 
+// -------------- LANGUAGE AREA -------------- //
+//This is where the tavern message will be stored
+let tavernMessage = "";
+
+window.addEventListener("load", function() {
+    if(localStorage.language) {
+        chosenLanguage = JSON.parse(localStorage.language);
+    };
+
+    $.getJSON("./language/"+chosenLanguage+".json", function(text){
+        tavernMessage = text.tavernMessage;
+    });
+});
+// -------------- END OF LANGUAGE AREA -------------- //
+
 // Getting the button TAVERN from the HTML
 const buttonTavern = document.getElementById("button__tavern");
 
 buttonTavern.addEventListener("click", function() {
     stats.health = stats.maxHealth;
     localStorage.setItem('stats', JSON.stringify(stats));
-    alert("You restored your HP to MAX!");
+    alert(tavernMessage);
     
     displayedHealth.classList.remove('red');
     /* *** ERASE THIS WHEN YOU CREATE A SEPARATE PAGE FOR THE TAVERN *** */
