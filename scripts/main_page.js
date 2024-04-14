997d46f0e351d397f2abeedb83ffd69dda5342b2
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2024-04-14 12:04:57 -0300

    Prettifying

--- scripts/main_page.js
+++ scripts/main_page.js
@@ -1,83 +1,83 @@
 // -------------- LANGUAGE AREA -------------- //
 // Getting the text in the explore button
-const exploreText = document.getElementById("button__explore__text");
+const exploreText = document.getElementById('button__explore__text')
 // Getting the text in the tavern button
-const tavernText = document.getElementById("button__tavern__text");
+const tavernText = document.getElementById('button__tavern__text')
 // Getting the text in the armory button
-const armoryText = document.getElementById("button__armory__text");
+const armoryText = document.getElementById('button__armory__text')
 // Getting the text in the stats title
-const statsTitle = document.getElementById("stats__title__text");
+const statsTitle = document.getElementById('stats__title__text')
 
 //This is where the warning texts will be stored (for new players)
-let exploreWarning = "";
-let tavernWarning = "";
+let exploreWarning = ''
+let tavernWarning = ''
 
-window.addEventListener("load", function() {
-    if(localStorage.language) {
-        chosenLanguage = JSON.parse(localStorage.language);
-    };
+window.addEventListener('load', function () {
+  if (localStorage.language) {
+    chosenLanguage = JSON.parse(localStorage.language)
+  }
 
-    $.getJSON("./language/"+chosenLanguage+".json", function(text){
-        tavernWarning = text.tavernWarning;
-        exploreWarning = text.exploreWarning;
-        exploreText.textContent = text.exploreBtn;
-        tavernText.textContent = text.tavernBtn;
-        armoryText.textContent = text.armoryBtn;
-        statsTitle.textContent = text.statsTitle;
-    });
-});
+  $.getJSON('./language/' + chosenLanguage + '.json', function (text) {
+    tavernWarning = text.tavernWarning
+    exploreWarning = text.exploreWarning
+    exploreText.textContent = text.exploreBtn
+    tavernText.textContent = text.tavernBtn
+    armoryText.textContent = text.armoryBtn
+    statsTitle.textContent = text.statsTitle
+  })
+})
 // -------------- END OF LANGUAGE AREA -------------- //
 
 // -------------- STATUS AREA -------------- //
 // Importing stats from main_stats.js
-let stats = JSON.parse(localStorage.getItem("stats"));
+let stats = JSON.parse(localStorage.getItem('stats'))
 
 // Getting the stats locations from the stats bar
-let displayedHealth = document.getElementById("stats__health");
-let displayedWeapon = document.getElementById("stats__weapon");
-let displayedCoins = document.getElementById("stats__coins");
+let displayedHealth = document.getElementById('stats__health')
+let displayedWeapon = document.getElementById('stats__weapon')
+let displayedCoins = document.getElementById('stats__coins')
 
 // Updating the stats
-window.addEventListener("load", function() {
-    if (stats.health < stats.maxHealth) {
-        displayedHealth.classList.add('red');
-    }
+window.addEventListener('load', function () {
+  if (stats.health < stats.maxHealth) {
+    displayedHealth.classList.add('red')
+  }
 
-    displayedCoins.classList.add('gold');
-    displayedWeapon.classList.add('grey');
+  displayedCoins.classList.add('gold')
+  displayedWeapon.classList.add('grey')
 
-    displayedHealth.textContent = stats.health;
-    displayedCoins.textContent = stats.coins;
-    displayedWeapon.textContent = stats.weapons[stats.weapons.length -1];
-});
+  displayedHealth.textContent = stats.health
+  displayedCoins.textContent = stats.coins
+  displayedWeapon.textContent = stats.weapons[stats.weapons.length - 1]
+})
 // -------------- END OF STATUS AREA -------------- //
 
 // -------------- NEW PLAYER AREA ----------------- //
 // Getting the explore button
-let explore = document.getElementById("button__explore");
+let explore = document.getElementById('button__explore')
 
 // Getting the tavern button
-let tavern = document.getElementById("button__tavern");
+let tavern = document.getElementById('button__tavern')
 
 // When the player clicks on the explore button before buying a weapon.
-explore.addEventListener("click", function() {
-    if (stats.weapons.includes('Dagger')) {
-        // If the player has a weapon
-        window.location.replace("./pages/explore.html");
-    } else {
-        // If the player doesn't have a weapon
-        window.location.replace("javascript:alert('"+exploreWarning+"')");
-    }
-});
+explore.addEventListener('click', function () {
+  if (stats.weapons.includes('Dagger')) {
+    // If the player has a weapon
+    window.location.replace('./pages/explore.html')
+  } else {
+    // If the player doesn't have a weapon
+    window.location.replace("javascript:alert('" + exploreWarning + "')")
+  }
+})
 
 // When the player clicks on the tavern button before buying a weapon.
-tavern.addEventListener("click", function() {
-    if (stats.weapons.includes('Dagger')) {
-        // If the player has a weapon
-        window.location.replace("./pages/tavern.html");
-    } else {
-        // If the player doesn't have a weapon
-        window.location.replace("javascript:alert('"+tavernWarning+"')");
-    }
-});
+tavern.addEventListener('click', function () {
+  if (stats.weapons.includes('Dagger')) {
+    // If the player has a weapon
+    window.location.replace('./pages/tavern.html')
+  } else {
+    // If the player doesn't have a weapon
+    window.location.replace("javascript:alert('" + tavernWarning + "')")
+  }
+})
 // -------------- END OF NEW PLAYER AREA ----------------- //
--- scripts/main_stats.js
+++ scripts/main_stats.js
@@ -1,13 +1,18 @@
 // When the window loads this call the function "setStats"
-window.addEventListener("load", setStats({
+window.addEventListener(
+  'load',
+  setStats({
     health: 5,
     maxHealth: 5,
     bag: [],
     weapons: [],
-    coins: 10
-}));
+    coins: 10,
+  })
+)
 
 // This is the function that will set all the stats (ONLY ONCE)
 function setStats(stats) {
-    localStorage.getItem("stats")? 0 : localStorage.setItem("stats", JSON.stringify(stats));
-};
+  localStorage.getItem('stats')
+    ? 0
+    : localStorage.setItem('stats', JSON.stringify(stats))
+}
