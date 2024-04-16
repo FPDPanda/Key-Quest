4152d1051989197cf17d2e4b9433540dc31dbbd7
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2024-04-15 23:10:11 -0300

    Adding button component to main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Button
+        id="explore"
+        text="Explore"
+        imageSource="./images/icons/explore.png"
+        alternativeText="crossed swords"
+      />
+      <Button
+        id="tavern"
+        text="Tavern"
+        imageSource="./images/icons/tavern.png"
+        alternativeText="beer mug"
+      />
+      <Button
+        id="armory"
+        text="Armory"
+        imageSource="./images/icons/armory.png"
+        alternativeText="hammer"
+      />
+    </div>
+  );
 }
 
 export default App;
