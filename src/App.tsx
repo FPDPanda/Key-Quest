3b5675dafb6a0566af052344b8db3a59f80fb906
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2024-04-16 18:49:41 -0300

    Importing styles to main page

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import MoveToAreaButton from "./components/MoveToAreaButton/MoveToAreaButton";
 
 function App() {
   return (
-    <div className="App">
+    <div className="game">
       <MoveToAreaButton
         id="explore"
         text="Explore"
