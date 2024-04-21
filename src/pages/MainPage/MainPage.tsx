36dcbf61d756ffe906244b540ec479fb971e30ef
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2024-04-21 14:25:08 -0300

    Adding new page for the Main App Page

---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from "react";
+import { MainPageWrapper } from "./MainPage.styled";
+import { Link } from "react-router-dom";
+import MoveToAreaButton from "../../components/MoveToAreaButton/MoveToAreaButton";
+import PlayerStats from "../../components/PlayerStats/PlayerStats";
+import Player from "../../components/Player/Player";
+
+interface MainPageProps {}
+
+const MainPage: FC<MainPageProps> = () => (
+  <MainPageWrapper>
+    <Link id="explore" to="/explore">
+      <MoveToAreaButton
+        text="Explore"
+        imageSource="./images/icons/explore.png"
+        alternativeText="crossed swords"
+      />
+    </Link>
+    <Link id="tavern" to="/tavern">
+      <MoveToAreaButton
+        text="Tavern"
+        imageSource="./images/icons/tavern.png"
+        alternativeText="beer mug"
+      />
+    </Link>
+    <Link id="armory" to="/armory">
+      <MoveToAreaButton
+        text="Armory"
+        imageSource="./images/icons/armory.png"
+        alternativeText="hammer"
+      />
+    </Link>
+    <PlayerStats />
+    <Player imageSource="./images/character/knight.gif" />
+  </MainPageWrapper>
+);
+
+export default MainPage;
