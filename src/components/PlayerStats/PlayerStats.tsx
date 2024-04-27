195e33866e177ed8f65d5aa34b86ebac0610bb46
Jean Jacques <35910266+FPDPanda@users.noreply.github.com>
2024-04-26 23:08:04 -0300

    PlayerStats - removing boilerplate and using stats from player

--- src/components/PlayerStats/PlayerStats.tsx
+++ src/components/PlayerStats/PlayerStats.tsx
@@ -6,33 +6,41 @@ import {
   PlayerStatsTitleText,
   PlayerStatsWrapper,
 } from "./PlayerStats.styled";
+import playerFeature from "../../features/Player";
 
 interface PlayerStatsProps {}
 
-const PlayerStats: FC<PlayerStatsProps> = () => (
-  <PlayerStatsWrapper>
-    <PlayerStatsTitleArea>
-      <PlayerStatsTitleText>Stats</PlayerStatsTitleText>
-    </PlayerStatsTitleArea>
-    <PlayerStatsStatImage
-      $gridArea="healthImg"
-      src="./images/icons/heart.png"
-    />
-    <PlayerStatsStatText $gridArea="healthText" color="black">
-      5
-    </PlayerStatsStatText>
-    <PlayerStatsStatImage $gridArea="coinsImg" src="./images/icons/coins.png" />
-    <PlayerStatsStatText $gridArea="coinsText" color="gold">
-      5
-    </PlayerStatsStatText>
-    <PlayerStatsStatImage
-      $gridArea="weaponImg"
-      src="./images/icons/dagger.png"
-    />
-    <PlayerStatsStatText $gridArea="weaponText" color="grey">
-      5
-    </PlayerStatsStatText>
-  </PlayerStatsWrapper>
-);
+const PlayerStats: FC<PlayerStatsProps> = function () {
+  const player = playerFeature.getPlayer();
+
+  return (
+    <PlayerStatsWrapper>
+      <PlayerStatsTitleArea>
+        <PlayerStatsTitleText>Stats</PlayerStatsTitleText>
+      </PlayerStatsTitleArea>
+      <PlayerStatsStatImage
+        $gridArea="healthImg"
+        src="./images/icons/heart.png"
+      />
+      <PlayerStatsStatText $gridArea="healthText" color="black">
+        {player.stats.currentHealth}/{player.stats.maxHealth}
+      </PlayerStatsStatText>
+      <PlayerStatsStatImage
+        $gridArea="coinsImg"
+        src="./images/icons/coins.png"
+      />
+      <PlayerStatsStatText $gridArea="coinsText" color="gold">
+        {player.coins}
+      </PlayerStatsStatText>
+      <PlayerStatsStatImage
+        $gridArea="weaponImg"
+        src="./images/icons/dagger.png"
+      />
+      <PlayerStatsStatText $gridArea="weaponText" color="grey">
+        {player.weapons[0]}
+      </PlayerStatsStatText>
+    </PlayerStatsWrapper>
+  );
+};
 
 export default PlayerStats;
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -5,34 +5,34 @@ import MoveToAreaButton from "../../components/MoveToAreaButton/MoveToAreaButton
 import PlayerStats from "../../components/PlayerStats/PlayerStats";
 import Player from "../../components/Player/Player";
 
-interface MainPageProps {}
-
-const MainPage: FC<MainPageProps> = () => (
-  <MainPageWrapper>
-    <Link id="explore" to="/explore">
-      <MoveToAreaButton
-        text="Explore"
-        imageSource="./images/icons/explore.png"
-        alternativeText="crossed swords"
-      />
-    </Link>
-    <Link id="tavern" to="/tavern">
-      <MoveToAreaButton
-        text="Tavern"
-        imageSource="./images/icons/tavern.png"
-        alternativeText="beer mug"
-      />
-    </Link>
-    <Link id="armory" to="/armory">
-      <MoveToAreaButton
-        text="Armory"
-        imageSource="./images/icons/armory.png"
-        alternativeText="hammer"
-      />
-    </Link>
-    <PlayerStats />
-    <Player imageSource="./images/character/knight.gif" />
-  </MainPageWrapper>
-);
+const MainPage: FC = function () {
+  return (
+    <MainPageWrapper>
+      <Link id="explore" to="/explore">
+        <MoveToAreaButton
+          text="Explore"
+          imageSource="./images/icons/explore.png"
+          alternativeText="crossed swords"
+        />
+      </Link>
+      <Link id="tavern" to="/tavern">
+        <MoveToAreaButton
+          text="Tavern"
+          imageSource="./images/icons/tavern.png"
+          alternativeText="beer mug"
+        />
+      </Link>
+      <Link id="armory" to="/armory">
+        <MoveToAreaButton
+          text="Armory"
+          imageSource="./images/icons/armory.png"
+          alternativeText="hammer"
+        />
+      </Link>
+      <PlayerStats />
+      <Player imageSource="./images/character/knight.gif" />
+    </MainPageWrapper>
+  );
+};
 
 export default MainPage;
